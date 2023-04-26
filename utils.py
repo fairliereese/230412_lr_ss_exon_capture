2955d451541fed063051e671f17bd36bd7c6fba2
fairliereese <fairliek@comcast.net>
2023-04-26 15:31:16 -0700

    forgot to add utils

---
+++ utils.py
@@ -0,0 +1,24 @@
+def reverse_alignment(infile, outfile, threads=1):
+
+    reverse_strand = {0: 16, 16: 0}
+
+    if infile.endswith('.bam'):
+        in_mode = 'rb'
+    else:
+        in_mode = 'r'
+    if outfile.endswith('bam'):
+        out_mode = 'wb'
+    else:
+        out_mode = 'w'
+    input =  pysam.AlignmentFile(infile, in_mode, threads=threads)
+    output = pysam.AlignmentFile(outfile, out_mode, template=input, threads=threads)
+
+    for read in input:
+        if read.has_tag('ts') and read.flag in reverse_strand:
+            if read.get_tag('ts') == '-':
+                read.flag = reverse_strand[read.flag]
+                read.set_tag('ts', '+')
+
+            output.write(read)
+    input.close()
+    output.close()
